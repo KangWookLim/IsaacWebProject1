a40bff70b18736f691281ba77c2badc6aa0e71e4
jo_sung <zounsam@naver.com>
2023-12-06 14:01:37 +0900

    setting

--- src/main/java/com/example/isaacwebproject/items/controller/ItemsController.java
+++ src/main/java/com/example/isaacwebproject/items/controller/ItemsController.java
@@ -1,22 +1,28 @@
 package com.example.isaacwebproject.items.controller;
 
 import com.example.isaacwebproject.items.service.ItemsService;
+import com.example.isaacwebproject.items.vo.Items;
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ItemsController {
     private final ItemsService itemsService;
-//    @GetMapping("/")
-//    public ModelAndView CollectionsList() {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("views/home");
-//        Collections collection = this.collectionsService.getCollection(1);
-//        modelAndView.addObject("collection", collection);
-//        return modelAndView;
-//    }
+    @RequestMapping("/shop")
+    public ModelAndView itemsList() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("views/shop");
+        List<Items> itemList = this.itemsService.getAllItems();
+        modelAndView.addObject("itemList", itemList);
+        return modelAndView;
+    }
 
 }
